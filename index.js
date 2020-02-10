b07f06684b776798f0f8569079ffc11c0fadecb6
Mark Adel <m.adel@owlsapp.net>
2020-02-10 16:24:20 +0200

    #0 - remove logs and comments

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ io.on('connection', (socket) => {
     console.log('new room id', roomId);
     await leaveAllRooms(socket);
     socket.join(roomId, () => {
-      console.log('joined rooms', Object.keys(socket.rooms));
       socket.emit('room join', { roomId, numberOfMembers: 1 });
     });
   });
@@ -22,7 +21,6 @@ io.on('connection', (socket) => {
     if (io.sockets.adapter.rooms[roomId]) {
       numberOfMembers = io.sockets.adapter.rooms[roomId].length;
     }
-    console.log('number of members 1', numberOfMembers);
     if (numberOfMembers < 2) {
       await leaveAllRooms(socket);
       socket.join(roomId, () => {
@@ -30,7 +28,6 @@ io.on('connection', (socket) => {
           numberOfMembers = io.sockets.adapter.rooms[roomId].length;
         }
         socket.emit('room join', { roomId, numberOfMembers });
-        console.log('number of members 2', numberOfMembers);
         if (numberOfMembers === 2) {
           // TODO: update analytics in a database
           io.to(roomId).emit('game start');
@@ -46,44 +43,33 @@ io.on('connection', (socket) => {
     io.to(roomId).emit('new move', { row, col, playerColor });
   });
 
-  // when a user wins
   socket.on('player win', (data) => {
     const { roomId, winningPlayer } = data;
     io.to(roomId).emit('player win', { winningPlayer });
-    // on client: show a message of winner and a button for a new game
   });
 
-  // when a user clicks new game
   socket.on('new game', (data) => {
     const { roomId } = data;
     io.to(roomId).emit('new game');
-    // on client: reset the view
   });
   
-  // when a user clicks cancel
   socket.on('game cancel', (data) => {
     const { roomId } = data;
     io.to(roomId).emit('game cancel');
-    // on client: clear the local storage and navigate to the home page
   });
 
   socket.on('user leave', async () => {
-    console.log('user left the game');
     const rooms = Object.keys(socket.rooms);
-    console.log('and his rooms was', rooms);
     for (let i = 0; i < rooms.length; i++) {
       socket.to(rooms[i]).emit('user leave');
     }
     await leaveAllRooms(socket);
-    console.log('and after removing him from rooms', Object.keys(socket.rooms));
   });
 
   socket.on('disconnecting', () => {
     const rooms = Object.keys(socket.rooms);
-    console.log('disconnecting client socket id', socket.id);
-    console.log('disconnecting client rooms', rooms);
     for (let i = 0; i < rooms.length; i++) {
-      socket.to(rooms[i]).emit('user leave');
+      io.to(rooms[i]).emit('user leave');
     }
   });
 });
